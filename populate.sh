5635320283de599748e71ec26c93606dcb2d0465
FredLoney <loneyf@ohsu.edu>
2018-03-16 10:43:53 -0700

    Clarify implementation pending message.

--- populate.sh
+++ populate.sh
@@ -1,5 +1,6 @@
 # Obtains the Nursa JSON files.
 cat <<HERE
-Pending Nursa RESTS API changes, the Nursa JSON files can be obtained
-by contacting loneyf@ohsu.edu.
+This Nursa Solr Indexer is not yet implemented pending Nursa REST
+API changes, the Nursa JSON files can be obtained by contacting
+loneyf@ohsu.edu.
 HERE
